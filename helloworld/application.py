8b8293daa00afd089e2ca75127a269f53c3ec194
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2021-04-05 16:52:38 +0000

    2

--- helloworld/application.py
+++ helloworld/application.py
@@ -3,8 +3,13 @@ import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
 import requests
+from flask_cors import CORS
+
+
 
 application = Flask(__name__)
+CORS(application, resources={r"/*": {"origins": "*"}})
+
 @application.route('/calc/bit', methods=['GET'])
 def post_currency_bit():
     return Response(json.dumps(get_bitcoin_index()), mimetype='application/json', status=200)
