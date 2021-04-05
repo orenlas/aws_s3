7b9d577125afd800240eff70d6c6552cda34701d
EC2 Default User <ec2-user@ip-172-31-54-206.ec2.internal>
2021-04-05 16:04:14 +0000

    added bitcoin support

--- helloworld/application.py
+++ helloworld/application.py
@@ -2,9 +2,16 @@
 import json
 from flask import Flask, Response
 from helloworld.flaskrun import flaskrun
+import requests
 
 application = Flask(__name__)
-
+@application.route('/calc/bit', methods=['GET'])
+def post_currency_bit():
+    return Response(json.dumps(get_bitcoin_index()), mimetype='application/json', status=200)
+def get_bitcoin_index():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice.json'
+    response = requests.get(url).json()['bpi']['USD']
+    return response
 @application.route('/', methods=['GET'])
 def get():
     return Response(json.dumps({'Output': 'Hello World'}), mimetype='application/json', status=200)
